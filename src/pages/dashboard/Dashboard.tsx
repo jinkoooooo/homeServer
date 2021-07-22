ab0c8f590ffe3f11a0fc34b1bb855fe1a13bb428
정진구 <jingoo.jeong@altiall.com>
2021-07-22 15:11:25 +0900

    test

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -32,31 +32,31 @@ function DashBoard() {
 
       <Grid container spacing={6}>
         <Grid item xs={12} sm={12} md={6} lg={3} xl>
-          안녕하세요. 정진구의 프로필 사이트입니다.
+          안녕하세요. 멋진구의 프로필 및 포트폴리오 사이트입니다.
         </Grid>
 
         <Grid item xs={12} sm={12} md={6} lg={3} xl>
-          2
+          저는 가람이를 사랑합니다♥
         </Grid>
 
         <Grid item xs={12} sm={12} md={6} lg={3} xl>
-          3
+          얼른 오늘(2021.7.22)이 지나가고 내일이 와서
         </Grid>
       </Grid>
 
       <Grid container spacing={6}>
         <Grid item xs={12} lg={8}>
-          4
+
         </Grid>
 
         <Grid item xs={12} lg={4}>
-          5
+          우리 이쁜 가람이를 보러 가고싶습니다.!!!!!
         </Grid>
       </Grid>
 
       <Grid container spacing={6}>
         <Grid item xs={12} lg={8}>
-          6
+          ♥3♥
         </Grid>
       </Grid>
     </React.Fragment>
